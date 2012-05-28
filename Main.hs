bf5a409342f8a1d1a9df439c7201be70e1b8ea3f
Adam Michalik <adam@michalik.es>
2012-05-28 17:00:59 +0200

    fixed main

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.IO
+import System.Environment
 
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
@@ -13,6 +14,7 @@ import TypeChecker
 import CPS
 import CodeGenerator
 import Assembler
+import Interpreter
 
 type CompilerProgram = MaybeT IO
 
@@ -31,14 +33,24 @@ typeCheck unit = case typeCheckTranslationUnit unit of
   Left err -> reportError (show err)
   Right _ -> return ()
 
-compile :: String -> CompilerProgram ()
-compile input = do
+parseAndTypecheck :: String -> CompilerProgram TranslationUnit
+parseAndTypecheck input = do
   unit <- parse input
   lift (hPutStrLn stderr ("PARSED CORRECTLY:\n\n" ++ show unit ++ "\n\n\n"))
 
   typeCheck unit
   lift (hPutStrLn stderr "TYPE CHECKING PASSED\n\n\n")
+  
+  return unit
 
+execute :: (TranslationUnit -> CompilerProgram ()) -> String -> CompilerProgram ()
+execute uh input = do
+  unit <- parseAndTypecheck input
+  uh unit
+  return ()
+
+compilator :: TranslationUnit -> CompilerProgram ()
+compilator unit = do
   let cpsBlocks = cpsTransformTranslationUnit unit
       snippets = generateFunsCode cpsBlocks
   lift (hPutStrLn stderr ("CPS BLOCKS:\n\n" ++ show cpsBlocks ++ "\n\n\n"))
@@ -48,10 +60,16 @@ compile input = do
   lift (putStrLn (show primitiveBlocks))
   return ()
 
+interpreter :: TranslationUnit -> CompilerProgram ()
+interpreter unit = do
+  lift (evalTranslationUnit unit)
+  return ()
+
 main :: IO ()
 main = do
+  args <- getArgs
   input <- getContents
-  result <- runMaybeT (compile input)
-  case result of
-    Just () -> hPutStrLn stderr "Everything all right"
-    Nothing -> hPutStrLn stderr "Errors, stopped"
+  if "-c" `elem` args
+    then runMaybeT (execute compilator input)
+    else runMaybeT (execute interpreter input)
+  return ()
--- tests/assign.bf
+++ tests/assign.bf
@@ -1,13 +1,31 @@
-int foo([int] a, bool b; locals int c; arrays;) {
-  if(b) {
-    a[c+2] = c*c;
-  } else if (c < 0) {
-    c = 2;
-  } else {};
-  return 2;
+int gcd(int a, int b; locals; arrays;) {
+  if(b == 0) {
+    return a;
+  } else {
+    b = call gcd(b, a%b);
+    return b;
+  };
 };
 
+void print_int_rev(int a; locals int b; arrays;) {
+  if(a == 0) {
+    output '0';
+  } else {
+    while(a > 0) {
+      output '0'+(a%10);
+      a = a/10;
+    };
+  };
+  return;
+};
+
+void init(; locals int a; arrays;) {
+  a = call gcd(60, 25);
+  call print_int_rev(a);
+  return;
+};
 
 void main(; locals; arrays;) {
+  call init();
   return;
 };
